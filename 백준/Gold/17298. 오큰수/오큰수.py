f19d0327973620ba57d9d50d4b5bc72dd2be89d6
juriHong <102223289+jurio3o@users.noreply.github.com>
2023-08-31 13:37:48 +0900

    [Gold IV] Title: 오큰수, Time: 1140 ms, Memory: 150704 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+stack = []
+answer = [-1 for i in range(n)]
+
+for i in range(len(arr)):
+    while stack and arr[stack[-1]] < arr[i]:
+        answer[stack.pop()] = arr[i]
+    stack.append(i)
+print(*answer)
+        
